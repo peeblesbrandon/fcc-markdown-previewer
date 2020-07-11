e4bd764368c36b0630a9013ff1a944b623c438cd
Brandon Peebles <peeblesbrandon@gmail.com>
2020-07-10 23:58:24 -0400

    Updates

--- precache-manifest.d25c2131a9528e56372c219efac66265.js
+++ precache-manifest.d25c2131a9528e56372c219efac66265.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a629f863b795f82686d9b6d34304e79",
+    "revision": "53780a3afd41b17086ae3c9fa8f0e6fa",
     "url": "/fcc-markdown-previewer/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fcc-markdown-previewer/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "3e3c404df6335edd440f",
+    "revision": "1a0d437474e3c6280606",
     "url": "/fcc-markdown-previewer/static/css/main.7a05d9b1.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fcc-markdown-previewer/static/js/2.6032fe09.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3e3c404df6335edd440f",
-    "url": "/fcc-markdown-previewer/static/js/main.40073d9a.chunk.js"
+    "revision": "1a0d437474e3c6280606",
+    "url": "/fcc-markdown-previewer/static/js/main.2c6e1e56.chunk.js"
   },
   {
     "revision": "71b3aad188744e00b565",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fcc-markdown-previewer/precache-manifest.ef30d78500abacc3abde73fb8601473e.js"
+  "/fcc-markdown-previewer/precache-manifest.d25c2131a9528e56372c219efac66265.js"
 );
 
 self.addEventListener('message', (event) => {
